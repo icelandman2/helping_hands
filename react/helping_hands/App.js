744da7aad2d685432feafee5760588a6d82706b7
Christina Li <tinali3698@gmail.com>
2019-04-22 16:02:52 -0700

    Added camera, prevent from hardcoding cards

--- react/helping_hands/App.js
+++ react/helping_hands/App.js
@@ -1,7 +1,9 @@
-import React from "react";
-import { View, Text , Image, StyleSheet} from "react-native";
+import React, { Component } from "react";
+import { AppRegistry, View, Text , Image, StyleSheet, Dimensions, TouchableOpacity} from "react-native";
 import { Button } from 'react-native-elements';
 import { createStackNavigator, createAppContainer } from "react-navigation";
+
+import {RNCamera} from 'react-native-camera';
 
 import SwipeCards from './SwipeCards.js'
 
@@ -40,7 +42,6 @@ class MenuScreen extends React.Component {
   render() {
     const {navigation} = this.props;
     const type = navigation.getParam('type', 'learn');
-
     return (
       <View style={styles.container}>
         <Text>{type}</Text>
@@ -92,17 +93,79 @@ class LearnScreen extends React.Component {
     }, 1500);
   }
 
+
   render() {
     const {navigation} = this.props;
     const type = navigation.getParam('type', 'learn');
     const sectionName = navigation.getParam('sectionName', 'alphabet');
 
+
     return (
       <View style={styles.container}>
         <Text>{type}</Text>
         <Text>{sectionName}</Text>
+        <Text>____{global.cards_left.toString()}___</Text>
         <Progress.Bar progress={parseFloat(global.curr_alphabet_cards)/parseFloat(global.total_alphabet_cards)} width={200} />
         <SwipeCards style={{flex: 1}} />
+        
+        <Button
+          containerStyle={styles.button}
+          title="Check sign"
+          onPress={() =>
+            this.props.navigation.push('Camera')}
+        />
+
+      </View>
+    );
+  }
+}
+
+class CameraScreen extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  takePicture = async function() {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+      this.props.navigation.push('Learn');
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+            <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          onGoogleVisionBarcodesDetected={({ barcodes }) => {
+            console.log(barcodes);
+          }}
+        />
+
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -112,7 +175,8 @@ const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
     Menu: MenuScreen,
-    Learn: LearnScreen
+    Learn: LearnScreen,
+    Camera: CameraScreen
   },
   {
     initialRouteName: "Home"
@@ -136,5 +200,20 @@ const styles = StyleSheet.create({
    },
    button: {
       padding:10
-   }
+   },
+    preview: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      height: Dimensions.get('window').height,
+      width: Dimensions.get('window').width
+    },
+    capture: {
+      flex: 0,
+      backgroundColor: '#fff',
+      borderRadius: 5,
+      color: '#000',
+      padding: 10,
+      margin: 40  
+    } 
 })
