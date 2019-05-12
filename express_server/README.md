bd75734d50af874c3fdbfa10d4b3fb16e0b82b34
ggarza5 <gabe84700@gmail.com>
2019-05-12 16:41:30 -0700

    Express server now listening on port 3000 with get endpoint /predictor executing gcloud/main.py and passing filename for prediction. soon, this file will be created from client data passed to the server. updated gcloud/main.py to parse filename from command line

---
+++ express_server/README.md
@@ -0,0 +1,11 @@
+Installing And Running Helping Hands Server Side
+1. sudo apt install python3-pip
+2. sudo apt-get install python3-venv
+3. git clone https://github.com/StanfordCS194/Team-5
+4. cd Team-5/express-server
+5. npm install
+6. . venv/bin/activate
+7. pip3 install flask numpy 
+8. pip3 install torch torchvision --no-cache-dir
+9. pip3 install sklearn --no-cache-dir
+10. node server.js & 
--- gcloud/main.py
+++ gcloud/main.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import Flask, jsonify
 import random
 import torch
 import torchvision
@@ -7,6 +7,8 @@ import sklearn
 import numpy as np
 import sklearn.metrics
 from PIL import Image
+import argparse
+import sys
 torch.manual_seed(7)
 
 model_path = '../classifier/exps/models/exp1.pt'
@@ -16,32 +18,38 @@ classes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D',
            'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
            'T', 'U', 'V', 'W', 'X', 'Y']
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--file', type=argparse.FileType('r'), help='PASS A FILE')
+
 def hello_get(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
-    """
+    with app.app_context():
+        """HTTP Cloud Function.
+        Args:
+            request (flask.Request): The request object.
+            <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+        Returns:
+            The response text, or any set of values that can be turned into a
+            Response object using `make_response`
+            <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+        """
+        model = torchvision.models.resnet18(pretrained = True)
+        num_ftrs = model.fc.in_features
+        num_classes = len(classes)
+        model.fc = torch.nn.Linear(num_ftrs, num_classes)
+        model.load_state_dict(torch.load(model_path))
+        options = vars(parser.parse_args())
+        if options['file']:
+            image_path = options['file'].name
+        else:
+            image_path = '../team_headshots/andrew.jpeg'
+        prediction = predictor(model, image_path)
+        print(prediction)
 
-    model = torchvision.models.resnet18(pretrained = True)
-    num_ftrs = model.fc.in_features
-    num_classes = len(classes)
-    model.fc = torch.nn.Linear(num_ftrs, num_classes)
-    model.load_state_dict(torch.load(model_path))
-    image_path = '../team_headshots/andrew.jpeg'
+        random_sign = list('abcdefghijklmnopqrstuvwxyz')
+        random_correct = [True, False]
 
-    prediction = predictor(model, image_path)
-    print(prediction)
-
-    random_sign = list('abcdefghijklmnopqrstuvwxyz')
-    random_correct = [True, False]
-
-    return jsonify(sign=random.choice(random_sign),
-                   correct=random.choice(random_correct))
+        return jsonify(sign=random.choice(random_sign),
+                       correct=random.choice(random_correct))
 
 
 def predictor(model, image_path):
@@ -68,3 +76,7 @@ def predictor(model, image_path):
     probs = torch.softmax(output, dim = 1)
     index = output.data.numpy().argmax()
     return classes[index]
+
+app=Flask(__name__)
+dummy='stasd'
+hello_get(dummy)    
