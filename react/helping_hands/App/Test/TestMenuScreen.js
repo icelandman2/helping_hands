26c6fbed97945987fe9d422c5b759d64f3619ed4
ggarza5 <gabe84700@gmail.com>
2019-05-29 12:51:06 -0700

    Re-organized App.js into component files

---
+++ react/helping_hands/App/Test/TestMenuScreen.js
@@ -0,0 +1,202 @@
+import React, { Component } from "react";
+import { AppRegistry, TouchableHighlight, View, Text, FlatList, Image, StyleSheet, Dimensions, TouchableOpacity} from "react-native";
+import { Button } from 'react-native-elements';
+
+export default class TestMenuScreen extends React.Component {
+
+  pressAlphabet= async function() {
+    this.props.navigation.push('Test', {
+              sectionName: 'Alphabet',
+              type: 'Test'
+            });
+    global.cards_left = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'];
+    global.curr_cards = 0;
+    global.total_cards = 26;
+
+    global.learned = [];
+    global.not_learned = [];
+    global.maybe_learned = [];
+
+  };
+
+  pressEtiquette= async function() {
+    this.props.navigation.push('Test', {
+              sectionName: 'Etiquette',
+              type: 'Test'
+            });
+    global.cards_left = ['hello', 'thanks', 'bye'];
+    global.curr_cards = 0;
+    global.total_cards = 3;
+
+    global.learned = [];
+    global.not_learned = [];
+    global.maybe_learned = [];
+  };
+
+  render() {
+    const {navigation} = this.props;
+    const type = navigation.getParam('type', 'Learn');
+    return (
+      <View style={styles.container}>
+        <View style={styles.topContainerStyle}>
+          <Text style={styles.headerText}>{type}</Text>
+        </View>
+        <View style={styles.moduleContainerStyle}>
+          <View style={styles.moduleButtonContainer}>
+            <View style={styles.CircleShapeView}>
+              <View style={styles.InnerCircleShapeView}>            
+                <TouchableOpacity onPress={this.pressAlphabet.bind(this)}>
+                  <Image
+                    style={styles.moduleButton}
+                    source={require('../../img/alphabet_signs/a.png')}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+            <TouchableOpacity 
+              containerStyle={styles.moduleButtonText}
+              onPress={this.pressEtiquette.bind(this)}
+            ><Text style={styles.moduleButtonText}>Alphabet</Text></TouchableOpacity>
+          </View>
+          <View style={styles.moduleButtonContainer}>
+            <View style={styles.CircleShapeView}>
+              <View style={styles.InnerCircleShapeView}>                        
+                <TouchableOpacity onPress={this.pressEtiquette.bind(this)}>
+                  <Image
+                    style={styles.moduleButton}
+                    source={require('../../img/thankyou.png')}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+            <TouchableOpacity 
+              containerStyle={styles.moduleButtonText}
+              title="Basic Etiquette"
+              onPress={this.pressEtiquette.bind(this)}
+            ><Text style={styles.moduleButtonText}>Basic Etiquette</Text></TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  baseText: {
+
+  },
+  topContainerStyle: {
+    // position: 'absolute',
+    top: 0,
+    alignItems: "center",
+    justifyContent: "center",   
+  },
+  headerText: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    margin: 20,
+  },
+  subHeaderText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+  paragraphText: {
+    fontSize: 14,
+    width: 250,
+    margin: 5,
+    textAlign: "center",
+  },
+  swipeCardsStyle: {
+    flex: 1,
+    height: 100,
+    zIndex: -1,
+  },
+  testQuestionStyle: {
+    fontSize: 64,
+    fontWeight: 'bold',
+    margin: 25,
+  },
+  bottomContainerStyle: {
+    position: 'absolute',
+    bottom: 15,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    top: 0,
+    left: 0,
+    textAlign: 'center',
+    // backgroundColor: '#e9ebee',
+  },
+  button: {
+    padding:10,
+  },
+  moduleContainerStyle: {
+    //alignItems: 'flex-end',
+    flex: 1, 
+    flexDirection: 'row', 
+    justifyContent: 'flex-end',
+  },
+  moduleButtonContainer: {
+    margin: 30,
+    marginTop: 0,
+    alignItems: "center",
+//    justifyContent: "center",
+  },
+  CircleShapeView: {
+    width: 120,
+    height: 120,
+    borderRadius: 120/2,
+    backgroundColor: '#00BCD4',
+    padding: 20,
+},
+  InnerCircleShapeView: {
+    width: 80,
+    height: 80,
+    borderRadius: 80/2,
+    backgroundColor: '#FFF',
+    // padding: 10, 
+    paddingLeft: 5, 
+    paddingTop: 10,  
+  },
+  moduleButton: {
+    width: 60,
+    height: 60,
+    // flex: 1,
+    borderRadius: 15,
+    alignSelf: 'center',
+    backgroundColor: '#fff',
+    // justifyContent: 'center',
+    //borderRadius: 75,
+  },
+  moduleButtonText: {
+    fontSize:14,
+    textDecorationLine: "underline",
+    marginTop: 5,
+    // backgroundColor: "transparent",
+
+  },
+  checkButton: {
+    padding: 10, 
+    width: 300,
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    //alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40  
+  },
+})
