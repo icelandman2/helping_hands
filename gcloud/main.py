f9531ea1fdc38d4641cfa37f9944f11f4758c7cb
ggarza5 <gabe84700@gmail.com>
2019-06-12 15:05:05 -0700

    quick change to main.py

--- gcloud/main.py
+++ gcloud/main.py
@@ -99,8 +99,7 @@ def predictor(model, image_path):
     # image.to(device)
 
     transformation = transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
+        transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
